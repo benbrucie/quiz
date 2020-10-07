4604e6b6d98c122277a9b756455442376fb93133
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-07 09:06:31 +0000

    Added question and score counters

--- quiz.js
+++ quiz.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -54,6 +56,9 @@ function getNewQuestion()
    }
 
 	 questionCounter++;
+   questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+
+
 	 const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 	 currentQuestion = availableQuestions[questionIndex];
 	 question.innerText = currentQuestion.question;
@@ -77,16 +82,25 @@ choices.forEach(choice => {
 		 const selectedChoice = e.target;
 		 const selectedAnswer = selectedChoice.dataset["number"];
 
-  const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+     const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-  selectedChoice.parentElement.classList.add(classToApply);
+     if (classToApply === 'correct'){
+       increamentScore(CORRECT_BONUS);
+     }
+     
+     selectedChoice.parentElement.classList.add(classToApply);
 
-  setTimeout(() => {
-    selectedChoice.parentElement.classList.remove(classToApply);
-    getNewQuestion();
-  }, 1000);
+     setTimeout(() => {
+       selectedChoice.parentElement.classList.remove(classToApply);
+       getNewQuestion();
+     }, 1000);
 
 	});
 });
+
+increamentScore = num => {
+  score +=num;
+  scoreText.innerText = score;
+}
 
 startQuiz();
