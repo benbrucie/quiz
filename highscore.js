9305f13558018a5c2ed8c7a82e551aa2851fa606
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-09 09:36:36 +0000

    highscore display debugged

--- highscore.js
+++ highscore.js
@@ -1,7 +1,9 @@
 //Reference: https://www.udemy.com/course/build-a-quiz-app-with-html-css-and-javascript/
+
 const highScoreList = document.getElementById("highScoreList");
-const highscore = JSON.parse(localStorage.getItem("highScore")) || [];
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 
 highScoreList.innerHTML = highScore.map(score => {
-    return '<li class="high-score">${score.name}-${score.score}</li>';
+    return '<li class="high-score">'+ score.name + '-' + score.score + '</li>';
   }).join("");
+//
